c409daef13a0869ec1d06e04ee1d266471c14238
DmitryMakarich <63462326+DmitryMakarich@users.noreply.github.com>
2022-03-15 18:00:41 +0300

    Todos[]: make main file in store

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/index.saga";
+import { store } from "./redux";
 
 ReactDOM.render(
   <React.StrictMode>
--- client/src/redux/index.saga.ts
+++ client/src/redux/index.saga.ts
@@ -1,7 +1,5 @@
-import { applyMiddleware, createStore } from "redux";
-import createSagaMiddleware from "redux-saga";
 import { fork } from "redux-saga/effects";
-import { rootReducer } from "./index.reducer";
+
 import watchTagFetching from "./tag/tag.saga";
 import watchTodoActions from "./todo/todo.saga";
 import watchUserAuthentication from "./user/user.saga";
@@ -12,8 +10,4 @@ export default function* rootSaga() {
   yield fork(watchTodoActions);
 }
 
-const sagaMiddleware = createSagaMiddleware();
 
-export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
-
-sagaMiddleware.run(rootSaga);
